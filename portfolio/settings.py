024fd0b3db67eadad26ad5466a71bcb2d92e07da
surya hangam <surya.limbu@infodevelopers.com.np>
2020-09-17 23:40:20 +0545

    add: allowed host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '4ogs&k21&%3%mlt=@r4(xs8=1-f!k07b762ws@_z#zxqq60*^@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['suryahangam.pythonanywhere.com']
 
 
 # Application definition
